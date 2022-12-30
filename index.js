d6b8e500292b1fe822b4b3f6ce201d481a691d9c
Marwa <marwa.al.shahal@gmail.com>
2022-12-30 10:52:12 +0200

    Step 3

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('ok')
 })
+app.get('/test', (req, res)=>{
+    res.json({"status":200,"message":"Ok"})
+});
+
+app.get('/time', (req, res)=>{
+    const d = new Date();
+    let hours = d.getHours();
+    let minutes = d.getMinutes()
+    res.json({"status":200,"message":`${hours}:${minutes}`})
+}); 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
