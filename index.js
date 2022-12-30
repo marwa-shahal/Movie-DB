8076f06706a2c1cb0c6b0d2a67eddbfa55340c8d
Marwa <marwa.al.shahal@gmail.com>
2022-12-30 12:16:36 +0200

    Step 7

--- index.js
+++ index.js
@@ -1,70 +1,88 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 
-app.get('/', (req, res) => {
-  res.send('ok')
-})
-app.get('/test', (req, res)=>{
-    res.json({"status":200,"message":"Ok"})
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+app.get("/test", (req, res) => {
+  res.json({ status: 200, message: "Ok" });
 });
 
-app.get('/time', (req, res)=>{
-    const d = new Date();
-    let hours = d.getHours();
-    let minutes = d.getMinutes()
-    res.json({"status":200,"message":`${hours}:${minutes}`})
-}); 
+app.get("/time", (req, res) => {
+  const d = new Date();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  res.json({ status: 200, message: `${hours}:${minutes}` });
+});
 
-app.get('/hello/:id', (req, res)=>{
-    res.json({"status":200,"message":"Hello,"+req.params.id})
-}); 
+app.get("/hello/:id", (req, res) => {
+  res.json({ status: 200, message: "Hello," + req.params.id });
+});
 
-app.get('/search', (req, res)=>{
-    const search = req.query.s;
-    typeof search != 'undefined'?res.json({status:200, message:"ok", data:search}):res.json({status:500, error:true, message:"you have to provide a search"}) 
-}); 
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  typeof search != "undefined"
+    ? res.json({ status: 200, message: "ok", data: search })
+    : res
+        .status(500)
+        .json({
+          status: 500,
+          error: true,
+          message: "you have to provide a search",
+        });
+});
 
-app.get('/movies/read', (req, res)=>{
-    res.json({status:200, data:movies})
-}); 
+app.get("/movies/read", (req, res) => {
+  res.json({ status: 200, data: movies });
+});
 
-app.get('/movies/read/by-date', (req, res)=>{
-    res.json({status:200, data:movies.sort((a,b)=> a.year - b.year)})
-}); 
+app.get("/movies/read/id/:id", (req, res) => {
+  const id = req.params.id;
+  id && id < movies.length
+    ? res.json({ status: 200, data: movies[id] })
+    : res
+        .status(404)
+        .json({
+          status: 404,
+          error: true,
+          message: "the movie <ID> does not exist",
+        });
+});
 
-app.get('/movies/read/by-rating', (req, res)=>{
-    res.json({status:200, data:movies.sort((a,b)=> b.rating - a.rating)})
-}); 
+app.get("/movies/read/by-date", (req, res) => {
+  res.json({ status: 200, data: movies.sort((a, b) => a.year - b.year) });
+});
 
-app.get('/movies/read/by-title', (req, res)=>{
-    res.json({status:200, data:movies.sort((a,b)=> {
-        let title_a = a.title.toUpperCase()
-        let title_b = b.title.toUpperCase()
-        let order = 0
-        title_a < title_b ? order = -1 : order = 1
-        return order;
-    })})
-}); 
+app.get("/movies/read/by-rating", (req, res) => {
+  res.json({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) });
+});
 
-app.get('/movies/create', (req, res)=>{
-   
-}); 
+app.get("/movies/read/by-title", (req, res) => {
+  res.json({
+    status: 200,
+    data: movies.sort((a, b) => {
+      let title_a = a.title.toUpperCase();
+      let title_b = b.title.toUpperCase();
+      let order = 0;
+      title_a < title_b ? (order = -1) : (order = 1);
+      return order;
+    }),
+  });
+});
 
-app.get('/movies/update', (req, res)=>{
-    
-}); 
+app.get("/movies/create", (req, res) => {});
 
-app.get('/movies/delete', (req, res)=>{
-    
-}); 
+app.get("/movies/update", (req, res) => {});
+
+app.get("/movies/delete", (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
