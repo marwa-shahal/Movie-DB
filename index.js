cb88d217fce251ba0a3eace0d04ee2ae7eb40281
Marwa <marwa.al.shahal@gmail.com>
2022-12-30 11:42:48 +0200

    Step 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -22,8 +28,22 @@ app.get('/hello/:id', (req, res)=>{
 
 app.get('/search', (req, res)=>{
     const search = req.query.s;
-    typeof search != 'undefined'?res.json({status:200, message:"ok", data:search}):res.json({status:500, error:true, message:"you have to provide a search"})
+    typeof search != 'undefined'?res.json({status:200, message:"ok", data:search}):res.json({status:500, error:true, message:"you have to provide a search"}) 
+}); 
+
+app.get('/movies/read', (req, res)=>{
+    res.json({status:200, data:movies})
+}); 
+
+app.get('/movies/create', (req, res)=>{
+   
+}); 
+
+app.get('/movies/update', (req, res)=>{
     
+}); 
+
+app.get('/movies/delete', (req, res)=>{
     
 }); 
 
