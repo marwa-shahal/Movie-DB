663590f80eb1d49c6904fb43b5a7d0527c8f3bd5
Marwa <marwa.al.shahal@gmail.com>
2022-12-31 23:56:02 +0200

    Step 12 half done

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
-const movies = [
-  { title: "Jaws", year: 1975, rating: 8 },
-  { title: "Avatar", year: 2009, rating: 7.8 },
-  { title: "Brazil", year: 1985, rating: 8 },
-  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
-];
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(
+  "mongodb+srv://admin:VWh6ABH43dCqt2do@cluster0.tzhvcg0.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+const UserSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 4,
+  },
+});
+
+const movies = mongoose.model("User", UserSchema);
+
+// const movies = [
+//   { title: "Jaws", year: 1975, rating: 8 },
+//   { title: "Avatar", year: 2009, rating: 7.8 },
+//   { title: "Brazil", year: 1985, rating: 8 },
+//   { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+// ];
 
 app.get("/", (req, res) => {
   res.send("ok");
@@ -38,14 +75,29 @@ app.get("/search", (req, res) => {
 });
 
 app.get("/movies/read", (req, res) => {
-  res.json({ status: 200, data: movies });
+  //res.json({ status: 200, data: movies });
+  movies
+    .find()
+    .then((moviesData) => {
+      res.send({ status: 200, data: moviesData });
+    })
+    .catch((err) => {
+      console.log("error");
+    });
 });
 
 app.get("/movies/read/id/:id", (req, res) => {
   const id = req.params.id;
   id && id < movies.length
-    ? res.json({ status: 200, data: movies[id] })
-    : res.status(404).json({
+    ? movies
+        .find()
+        .then((moviesData) => {
+          res.send({ status: 200, data: moviesData[id] });
+        })
+        .catch((err) => {
+          console.log("error");
+        })
+    : res.status(404).send({
         status: 404,
         error: true,
         message: "the movie <ID> does not exist",
@@ -53,68 +105,100 @@ app.get("/movies/read/id/:id", (req, res) => {
 });
 
 app.get("/movies/read/by-date", (req, res) => {
-  res.json({ status: 200, data: movies.sort((a, b) => a.year - b.year) });
+  movies
+        .find()
+        .then((moviesData) => {
+          res.send({ status: 200, data: movies.sort((a, b) => a.year - b.year) });
+        })
+        .catch((err) => {
+          console.log("error");
+        })
 });
 
 app.get("/movies/read/by-rating", (req, res) => {
-  res.json({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) });
+  movies
+        .find()
+        .then((moviesData) => {
+          res.send({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) });
+        })
+        .catch((err) => {
+          console.log("error");
+        })
 });
 
 app.get("/movies/read/by-title", (req, res) => {
-  res.json({
-    status: 200,
-    data: movies.sort((a, b) => {
-      let title_a = a.title.toUpperCase();
-      let title_b = b.title.toUpperCase();
-      let order = 0;
-      title_a < title_b ? (order = -1) : (order = 1);
-      return order;
-    }),
-  });
+  movies
+  .find()
+  .then((moviesData) => {
+    res.send({
+      status: 200,
+      data: movies.sort((a, b) => {
+        let title_a = a.title.toUpperCase();
+        let title_b = b.title.toUpperCase();
+        let order = 0;
+        title_a < title_b ? (order = -1) : (order = 1);
+        return order;
+      })})
+  .catch((err) => 
+    console.log("error")
+  )
+})
 });
-
 app.post("/movies/create", (req, res) => {
   req.query.rating ? req.query.rating : (req.query.rating = "4");
   let title = req.query.title;
   let year = req.query.year;
   let rating = req.query.rating;
   if (!title || !year || year.length != 4 || isNaN(year)) {
-    res.json({
+    res.send({
       status: 403,
       error: true,
       message: "you cannot create a movie without providing a title and a year",
     });
   } else {
-    movies.push({
-      title: title,
-      year: parseInt(year),
-      rating: parseInt(rating),
-    });
-    res.json({ status: 200, data: movies });
+    movies
+      .create({
+        title: title,
+        year: parseInt(year),
+        rating: parseInt(rating),
+      })
+      .then((newMovie) => {
+        movies
+          .find()
+          .then((moviesData) => {
+            res.send({ status: 200, data: moviesData });
+          })
+          .catch((err) => {
+            console.log("error, no entry found");
+          });
+      })
+      .catch((err) => {
+        "error, connot create element";
+      });
   }
 });
 
 app.put("/movies/update/:id", (req, res) => {
-    let id = req.params.id;
-    let title = req.query.title;
-    let rating = req.query.rating;
-    let year = req.query.year;
-    title ? movies[id-1].title = title : movies[id-1].title;
-    rating ? movies[id-1].rating = parseInt(rating) : "";
-    year ? movies[id-1].year = parseInt(year) : "";
-    res.json({ status: 200, data: movies });
+  let id = req.params.id;
+  let title = req.query.title;
+  let rating = req.query.rating;
+  let year = req.query.year;
+  title ? (movies[id - 1].title = title) : movies[id - 1].title;
+  rating ? (movies[id - 1].rating = parseInt(rating)) : "";
+  year ? (movies[id - 1].year = parseInt(year)) : "";
+  res.json({ status: 200, data: movies });
 });
 
 app.delete("/movies/delete/:id", (req, res) => {
   let id = req.params.id;
   if (id > movies.length) {
-    res.status(404).json({
+    res.status(404).send({
       status: 404,
       error: true,
       message: "the movie <ID> does not exist",
     });
   } else {
-    movies.splice(id-1, 1);
+    movies.splice(id - 1, 1);
     res.json({ status: 200, data: movies });
   }
 });
@@ -122,3 +206,5 @@ app.delete("/movies/delete/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
